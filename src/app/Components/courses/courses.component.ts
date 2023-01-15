983229790bb3a71ef43663cff8427ef97652a560
m7md6799 <m7md@gmail.com>
2023-01-15 18:35:38 +0200

    finish courses page

--- src/app/Components/courses/courses.component.ts
+++ src/app/Components/courses/courses.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { DataService } from 'src/app/Services/data.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent  implements OnInit{
+
+  Courses:any;
+  
+  constructor(private _DataService:DataService){}
+  ngOnInit(): void {
+    this._DataService.getData().subscribe({
+      next:(res:any)=> this.Courses = res.Courses,
+      error:(err:any)=>console.log(err)
+    })
+  }
 
 }
