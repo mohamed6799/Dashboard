fa007936ce2e1b7fa21bc509ecffcf63516e137a
m7md6799 <m7md@gmail.com>
2023-01-15 17:14:29 +0200

    finish projects page

--- src/app/Components/projects/projects.component.ts
+++ src/app/Components/projects/projects.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { DataService } from 'src/app/Services/data.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
-
+export class ProjectsComponent  implements OnInit{
+  Projects:any;
+  
+  constructor(private _DataService:DataService){}
+  ngOnInit(): void {
+    this._DataService.getData().subscribe({
+      next:(res:any)=> this.Projects = res.Projects,
+      error:(err:any)=>console.log(err)
+    })
+  }
 }
